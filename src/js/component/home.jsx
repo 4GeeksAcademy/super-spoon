0a5b7a4ed593a61cb6579f4ea689a75789edb22f
carolinaborr <162605159+carolinaborr@users.noreply.github.com>
2024-12-05 16:34:42 +0000

    pa isra

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	//const [inputValue, setInputValue] = useState("");
+	const [userName, setUserName] = useState([]);
+
+	const handlechange = (e) =>{
+		setUserName(e.target.value)
+	}
+
+console.log("user", userName);
+
+	const PostData =() => {
+		fetch('https://playground.4geeks.com/todo/users/CarolinaTodo', {method:POST})
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(error => console.error(error()))
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="Container">
+			<h1>My Neobrustalistic Todo List</h1>
+					<input type="text" onChange={handlechange}/>
+					<button onClick={() =>PostData()}>Post</button>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,11 @@ import ReactDOM from "react-dom/client";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 //import your own components
 import Home from "./component/home.jsx";
 
 //render your react application
 ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
